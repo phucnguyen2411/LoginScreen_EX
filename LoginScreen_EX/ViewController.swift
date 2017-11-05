cc88c3fe3725e53dab6a7b010408a071df5eeb5d
Phuc .Ng <phucnguyen.2411@gmail.com>
2017-11-05 12:06:10 +0700

    Initial Commit

---
+++ LoginScreen_EX/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LoginScreen_EX
+//
+//  Created by Phuc Nguyen on 11/5/17.
+//  Copyright © 2017 Phuc Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
